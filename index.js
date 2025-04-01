498d73512e948d811933847acdcbff7744bab363
Yury V <kotden09@gmail.com>
2025-04-01 13:02:50 +0300

    feat: implementation of task - "miMax"

--- index.js
+++ index.js
@@ -397,6 +397,30 @@ function sortOddDigit (arr) {
 
 }
 // Примеры:
-console.log(sortOddDigit([7, 1]))  //=>  [1, 7]
-console.log(sortOddDigit([5, 8, 6, 3, 4]))  //=>  [3, 8, 6, 5, 4]
-console.log(sortOddDigit([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))  //=>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+// console.log(sortOddDigit([7, 1]))  //=>  [1, 7]
+// console.log(sortOddDigit([5, 8, 6, 3, 4]))  //=>  [3, 8, 6, 5, 4]
+// console.log(sortOddDigit([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))  //=>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// 25)
+// Написать функцию, которая будет возвращать количество отдельных нечувствительных к регистру буквенных символов и числовых цифр,
+// которые встречаются во входной строке более одного раза. Предполагается, что входная строка содержит только буквы (как в верхнем, так и в нижнем регистре) и цифры.
+
+// Примеры
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+// 26)
+// Написать функцию, которая возвращает минимальное и максимальное количество заданного списка / массива.
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)];
+}
+// Примеры
+ console.log(minMax([1,2,3,4,5]))  // == [1,5]
+ console.log(minMax([2334454,5])) //== [5, 2334454]
+ console.log(minMax([1]))         //   == [1, 1]
